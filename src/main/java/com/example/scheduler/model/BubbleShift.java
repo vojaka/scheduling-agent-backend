fd8de3a35b01b3ff6c1ffa5e87dcc3fe5b2dcb3e
vojaka <kim.smirnov@gmail.com>
2026-06-28 01:10:55 +0300

    Upload src/main/java/com/example/scheduler/model/BubbleShift.java via scheduling agent middleware

--- src/main/java/com/example/scheduler/model/BubbleShift.java
+++ src/main/java/com/example/scheduler/model/BubbleShift.java
@@ -1,6 +1,7 @@
 package com.example.scheduler.model;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -10,6 +11,7 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 @JsonIgnoreProperties(ignoreUnknown = true)
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class BubbleShift {
     @JsonProperty("_id")
     private String id;
