4763f0f88dee2bb5200ea228712638935dd8a47b
vojaka <kim.smirnov@gmail.com>
2026-06-28 01:29:08 +0300

    Upload src/main/java/com/example/scheduler/model/BubbleUser.java via scheduling agent middleware

--- src/main/java/com/example/scheduler/model/BubbleUser.java
+++ src/main/java/com/example/scheduler/model/BubbleUser.java
@@ -1,6 +1,7 @@
 package com.example.scheduler.model;
 
 import com.fasterxml.jackson.annotation.JsonAlias;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
@@ -23,6 +24,7 @@ public class BubbleUser {
     @JsonAlias({"role", "role_list_option___user_role", "primary_login_role_option___user_role"})
     private Object roleObject;
 
+    @JsonIgnore
     public String getRole() {
         if (roleObject == null) {
             return "Worker";
